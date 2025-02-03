7dc39692864be47f942fdd6db6dd857bd2b7beb8
唐伟博 <tangweibo@192.168.10.149>
2025-02-03 21:38:25 +0800

    fix: simplify next.js configuration

--- next.config.js
+++ next.config.js
@@ -23,31 +23,11 @@ const nextConfig = {
       ...config.resolve.alias,
       '@': require('path').resolve(__dirname, 'src'),
     }
-    // 添加代码分割优化
-    config.optimization = {
-      ...config.optimization,
-      splitChunks: {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 24000000, // 确保分割后的文件小于 25MB
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-            priority: 10,
-          },
-          common: {
-            minChunks: 2,
-            name: 'common',
-            chunks: 'all',
-            priority: 1,
-          },
-        },
-      },
-    };
     return config;
   },
+  experimental: {
+    optimizePackageImports: ['antd']
+  }
 }
 
 module.exports = nextConfig 
