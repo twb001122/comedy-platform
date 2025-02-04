e259eb324de9b085a95c713b8ed25e39ba6dfb53
唐伟博 <tangweibo@192.168.10.149>
2025-02-04 18:03:24 +0800

    修复 tailwindcss 模块缺失问题

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 确保路径正确
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
